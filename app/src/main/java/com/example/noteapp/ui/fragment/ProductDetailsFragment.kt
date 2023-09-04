2eff138e56e29c9999e96147f6a55f10a4918136
PrincessGitu <gitanjalighangale007@gmail.com>
2023-09-04 15:46:04 +0530

    Add storage permission

--- app/src/main/java/com/example/noteapp/ui/fragment/ProductDetailsFragment.kt
+++ app/src/main/java/com/example/noteapp/ui/fragment/ProductDetailsFragment.kt
@@ -1,17 +1,34 @@
 package com.example.noteapp.ui.fragment
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.provider.MediaStore
+import android.provider.MediaStore.Images
+import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
+
 import com.example.noteapp.adapter.ProductImageAdapter
 import com.example.noteapp.databinding.FragmentProductDetailsBinding
 import com.example.noteapp.model.Product
@@ -20,20 +37,24 @@ import com.example.noteapp.utils.Resource
 import com.example.noteapp.viewModels.NeoProductDetailsViewModel
 import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+import java.io.IOException
+import java.net.URL
 
 
 @AndroidEntryPoint
 class ProductDetailsFragment : Fragment() {
-
 
     private var _binding: FragmentProductDetailsBinding? = null
     private val binding: FragmentProductDetailsBinding get() = _binding!!
 
     private lateinit var imageAdapter: ProductImageAdapter
 
-    lateinit var imageUrl:String
-
-
+    //picked image uri will be saved in it
+    private var imageUri: Uri? = null
+    private lateinit var imgUrl:String
     private val productDetailsViewModel by viewModels<NeoProductDetailsViewModel>()
 
     override fun onCreateView(
@@ -60,23 +81,23 @@ class ProductDetailsFragment : Fragment() {
         bindObserverData()
 
         binding.productDetailSharing.setOnClickListener {
-            val uri = Uri.parse(imageUrl)
-
-              shareImage(imageUrl)
+            startStoragePermissionRequest()
         }
     }
 
 
-    private fun shareImage(uriToImage: String) {
-        val shareIntent = Intent()
-        shareIntent.action = Intent.ACTION_SEND
-      //  shareIntent.putExtra(Intent.EXTRA_STREAM, uriToImage)
-       // shareIntent.type = "image/jpeg"
-        shareIntent. putExtra(Intent.EXTRA_TEXT, uriToImage)
-        shareIntent. type = "text/plain"
-        startActivity(Intent.createChooser(shareIntent, null))
+    // Function to check and request permission.
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            getUri()
+        } else {
+            Toast.makeText(requireContext(),"Permission Denied by user",Toast.LENGTH_SHORT).show()
+        }
     }
 
+    private fun startStoragePermissionRequest() {
+        requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    }
 
 
     private fun bindObserverData() {
@@ -110,13 +131,10 @@ class ProductDetailsFragment : Fragment() {
                             adapter = imageAdapter
                         }
                         val url = pData.product_images[0].image
-                        imageUrl=pData.product_images[0].image
+                        imgUrl=pData.product_images[0].image
                         Glide.with(requireContext()).load(url).into(binding.productDetailImage)
 
-
                     }
-
-
                 }
             }
         })
@@ -124,8 +142,32 @@ class ProductDetailsFragment : Fragment() {
 
 
     private fun onImageClick(imageData: ProductImage, position: Int) {
-        imageUrl=imageData.image
+        imgUrl=imageData.image
         Glide.with(requireContext()).load(imageData.image).into(binding.productDetailImage)
+
+    }
+
+    private fun getUri(){
+        lifecycleScope.launch(Dispatchers.IO){
+            try {
+                val url = URL(imgUrl)
+                val loadedImage = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+                val path: String = Images.Media.insertImage(requireContext().contentResolver, loadedImage, "", null)
+                imageUri = Uri.parse(path)
+                shareImage()
+            } catch (e: IOException) {
+                Log.e("shareImage","="+e.message)
+                Toast.makeText(requireContext(), "" + e.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+    private fun shareImage() {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.putExtra(Intent.EXTRA_TEXT, "Hey view/download this image")
+        intent.putExtra(Intent.EXTRA_STREAM, imageUri)
+        intent.type = "image/*"
+        startActivity(Intent.createChooser(intent, "Share image via..."))
 
     }
 
